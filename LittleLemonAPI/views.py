a2a9a9c365df114c27924aab9efce2231cb3955f
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-03 20:01:21 +0300

    create views

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Category,MenuItem,Cart,Order,OrderItem
+from .serializers import CategorySerializer,MenuItemSerializer
 
 # Create your views here.
+class CategoryView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
