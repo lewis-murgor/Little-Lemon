392d77bb7011d84a442e0135ca08beddf00f32a6
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-07 20:17:21 +0300

    create auth token url

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('category', views.CategoryView.as_view()),
+    path('api-token-auth/', obtain_auth_token),
 ]
