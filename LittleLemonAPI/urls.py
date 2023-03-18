22da0edf5a8f1234b3bf70e15ce8530959303be5
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-18 13:09:25 +0300

    create menu items url

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -4,5 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('category', views.CategoryView.as_view()),
+    path('menu-items', views.MenuItemsView.as_view()),
     path('api-token-auth/', obtain_auth_token),
 ]
