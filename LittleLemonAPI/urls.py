60e43156968b506dbbd73a2e34f4f8cc208a262a
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-18 22:02:08 +0300

    modify single menu item url

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('category', views.CategoryView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
-    path('menu-items/<int:id>', views.MenuItem.as_view()),
+    path('menu-items/<int:pk>', views.MenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token),
 ]
