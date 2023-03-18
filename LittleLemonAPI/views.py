a856eb9566b808336b7dc0fa24972ab539c65dd8
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-18 19:33:28 +0300

    create menu items get and post view

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import generics
+from rest_framework.views import APIView
 from .models import Category,MenuItem,Cart,Order,OrderItem
 from .serializers import CategorySerializer,MenuItemSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -9,6 +12,17 @@ class CategoryView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class MenuItemsView(generics.ListAPIView):
-    queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+class MenuItemsView(APIView):
+    def get(self, request):
+        queryset = MenuItem.objects.all()
+        serializers = MenuItemSerializer(queryset, many=True)
+        return Response(serializers.data)
+    
+    def post(self, request):
+        if request.user.groups.filter(name='Manager').exists():
+            serializers = MenuItemSerializer(data=request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data, status=status.HTTP_201_CREATED)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message":"You are not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
