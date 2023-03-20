b3c09c00b37f01bd21d3a4a6f4417079c3f49e99
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-20 17:35:17 +0300

    create single manager view url

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('menu-items', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.MenuItemView.as_view()),
     path('groups/manager/users', views.ManagerView.as_view()),
+    path('groups/manager/users/<int:pk>', views.SingleManagerView.as_view()),
     path('api-token-auth/', obtain_auth_token),
 ]
