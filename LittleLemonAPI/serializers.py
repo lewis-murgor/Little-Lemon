7034092cf9a1263b410ce17bd751df3f2c1fa62c
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-20 16:11:47 +0300

    create user serializer

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Category,MenuItem,Cart,Order,OrderItem
+from django.contrib.auth.models import User
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,27 @@ class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuItem
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(
+        style={"input_type": "password"}, write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'username', 'password', 'password2')
+
+        def save(self):
+            user = User(
+            email=self.validated_data['email'],
+            username=self.validated_data['username'],
+
+            )
+            password = self.validated_data['password']
+            password2 = self.validated_data['password2']
+
+            if password != password2:
+                raise serializers.ValidationError(
+                    {'password': 'Passwords do not match.'})
+            user.set_password(password)
+            user.save()
+            return user
