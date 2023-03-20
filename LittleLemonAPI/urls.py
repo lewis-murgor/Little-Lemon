cca6a1587598223d297ca5c4881340a181cbd189
Lewis <lewis.murgor@student.moringaschool.com>
2023-03-20 16:12:57 +0300

    create manager view url

--- LittleLemonAPI/urls.py
+++ LittleLemonAPI/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('category', views.CategoryView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
     path('menu-items/<int:pk>', views.MenuItemView.as_view()),
+    path('groups/manager/users', views.ManagerView.as_view()),
     path('api-token-auth/', obtain_auth_token),
 ]
