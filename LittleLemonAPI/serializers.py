0a52924dcfa63c87ab8330761c973e7b88b458f1
Lewis <lewis.murgor@student.moringaschool.com>
2023-04-04 20:21:26 +0300

    modify order serializer and order item serializer

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -27,12 +27,18 @@ class CartSerializer(serializers.ModelSerializer):
 
         read_only_fields = ["user"]
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
-
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True, read_only=True)
+    class Meta:
+        model = Order
+        fields = ('id', 'user', 'delivery_crew', 'status', 'total', 'order_items')
+
+        read_only_fields = ["user"]
+
+    
+
