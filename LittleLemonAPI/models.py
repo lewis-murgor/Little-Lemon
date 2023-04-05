bb160de899a818566324224e1bed2489da4a65a1
Lewis <lewis.murgor@student.moringaschool.com>
2023-04-05 19:27:36 +0300

    modify status field in order model

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -25,7 +25,7 @@ class Cart(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True)
-    status = models.BooleanField(db_index=True, default=True)
+    status = models.BooleanField(db_index=True, default=0)
     total = models.DecimalField(max_digits=6, decimal_places=2)
     date = models.DateField(db_index=True,auto_now_add=True)
 
